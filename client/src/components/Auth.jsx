d1eb4b303b43fe6c97fb7b6f53511ad7417e4caf
Anupam Dubey <9430137649mk@gmail.com>
2022-02-11 20:12:44 +0530

    Added backend url

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -32,7 +32,7 @@ const Auth = () => {
         e.preventDefault();
 
         const { username, password, phoneNumber, avatarURL } = form;
-        const URL = "http://localhost:5000/auth";
+        const URL = "https://mkbserver.herokuapp.com/auth";
         const { data: {token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? "signup" : "login" }`, {
             username, 
             password, 
